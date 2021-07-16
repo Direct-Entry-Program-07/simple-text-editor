3df8eb6b2bcfa9e8b2314d5d9be7fe8e22c8d198
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-07-16 19:48:52 +0530

    Added status text to the project status bar

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -5,16 +5,20 @@
 
 package controller;
 
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableBooleanValue;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.print.PrinterJob;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
 import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -37,6 +41,9 @@ public class EditorFormController {
     private final int searchIndex = 0;
     public TextArea txtEditor;
     public AnchorPane pneWorkingArea;
+    public ToolBar tlbrStatus;
+    public Label lblStatus;
+    public Label lblApplicationStatus;
     private Font editorFont ;
     public TextField txtFind;
     public AnchorPane pneFind;
@@ -53,13 +60,18 @@ public class EditorFormController {
     private final int endPoint = 0;
     private PrinterJob printerJob;
     private String absolutePath = "";
+    private String statusText = "";
+    private long allCount = 0;
+    private long numberCount = 0;
 
     public void initialize() {
         setEditorFont(Font.getDefault());
-        //setEditorFont(Font.font("Ubuntu"));
         pneFind.setVisible(false);
         pneReplace.setVisible(false);
         this.printerJob = PrinterJob.createPrinterJob();
+        lblApplicationStatus.setText("| Sample Text Loaded");
+
+        fillStatusBar((long) txtEditor.getText().length(), getNumbersCountInText());
 
         ChangeListener textListener = (ChangeListener<String>) (observable, oldValue, newValue) -> {
             searchMatches(newValue);
@@ -67,12 +79,42 @@ public class EditorFormController {
 
         txtSearch.textProperty().addListener(textListener);
         txtFind.textProperty().addListener(textListener);
-
-       // System.out.println(txtEditor.getFont().getName());
-        //txtEditor.setFont(new Font(12));
         txtEditor.setFont(editorFont);
         String changedFontName = FormatFontFormController.getChangedFontName();
-        System.out.println(changedFontName);
+        //System.out.println(changedFontName);
+
+        txtEditor.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                allCount = txtEditor.getText().length();
+                //System.out.println(allCount);
+                fillStatusBar(allCount, getNumbersCountInText());
+            }
+        });
+
+
+    }
+
+    private Long getNumbersCountInText(){
+        String trimmedText = txtEditor.getText().trim();
+        Long count = 0L;
+        char c;
+        for (int i = 0; i < trimmedText.length(); i++) {
+            c = trimmedText.charAt(i);
+            try{
+                Integer.parseInt(String.valueOf(trimmedText.charAt(i)));
+                count++;
+            }catch (RuntimeException e){
+
+            }
+        }
+
+        return count;
+    }
+
+    private void fillStatusBar(Long allCount, Long numberCount) {
+
+            lblStatus.setText("Editor contains " + " " + allCount + " Characters with "  + numberCount + " numbers");
 
     }
 
@@ -102,6 +144,8 @@ public class EditorFormController {
         txtEditor.clear();
         absolutePath = "";
         txtEditor.requestFocus();
+        lblApplicationStatus.setText("loaded new document.");
+
     }
 
     public void mnuItemExit_OnAction(ActionEvent actionEvent) {
@@ -130,22 +174,6 @@ public class EditorFormController {
     }
 
     public void btnFindNext_OnAction(ActionEvent actionEvent) {
-        /*if (!searchList.isEmpty()) {
-            if (findOffSet == -1) {
-                findOffSet = 0;
-            }
-            txtEditor.selectRange(searchList.get(findOffSet).getStartIndex(), searchList.get(findOffSet).getEndIndex());
-            selectedText = txtEditor.getSelectedText();
-            txtEditor.setStyle("-fx-highlight-fill: #fff500;");
-            startPoint = searchList.get(findOffSet).getStartIndex();
-            endPoint = searchList.get(findOffSet).getEndIndex();
-
-            findOffSet++;
-            if (findOffSet >= searchList.size()) {
-                findOffSet = 0;
-            }
-        }*/
-
         if (!searchList.isEmpty()) {
             findOffSet++;
             if (findOffSet >= searchList.size()) {
@@ -156,16 +184,6 @@ public class EditorFormController {
     }
 
     public void btnFindPrevious_OnAction(ActionEvent actionEvent) {
-        /*if (!searchList.isEmpty()) {
-            if (findOffSet == -1) {
-                findOffSet = searchList.size() - 1;
-            }
-            txtEditor.selectRange(searchList.get(findOffSet).getStartIndex(), searchList.get(findOffSet).getEndIndex());
-            findOffSet--;
-            if (findOffSet < 0) {
-                findOffSet = searchList.size() - 1;
-            }
-        }*/
 
         if (!searchList.isEmpty()) {
             findOffSet--;
@@ -177,27 +195,12 @@ public class EditorFormController {
     }
 
     public void btnReplace_OnAction(ActionEvent actionEvent) {
-        /*if (!selectedText.isEmpty()) {
-            selectedText = txtReplaceText.getText();
-            txtEditor.replaceText(startPoint, endPoint, txtReplaceText.getText());
-        }*/
         if (findOffSet == -1) return;
         txtEditor.replaceText(searchList.get(findOffSet).getStartIndex(), searchList.get(findOffSet).getEndIndex(), txtReplaceText.getText());
         searchMatches(txtFind.getText());
     }
 
     public void btnReplaceAll_OnAction(ActionEvent actionEvent) {
-        /*for (Index index : searchList) {
-            txtEditor.selectRange(index.getStartIndex(), index.getEndIndex());
-            selectedText = txtEditor.getSelectedText();
-
-            if (!selectedText.isEmpty()) {
-                txtEditor.setStyle("-fx-highlight-fill: #fff500;");
-                selectedText = txtReplaceText.getText();
-                txtEditor.replaceText(index.getStartIndex(), index.getEndIndex(), txtReplaceText.getText());
-            }
-        }*/
-
 
         while (!searchList.isEmpty()) {
             txtEditor.replaceText(searchList.get(0).getStartIndex(), searchList.get(0).getEndIndex(), txtReplaceText.getText());
@@ -227,17 +230,8 @@ public class EditorFormController {
 
         txtEditor.clear();
         absolutePath = file.getAbsolutePath();
-        System.out.println(absolutePath);
-        try (FileReader fileReader = new FileReader(file);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                txtEditor.appendText(line + '\n');
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        //System.out.println(absolutePath);
+        openFile(file);
     }
 
     public void openFile(File file){
@@ -247,6 +241,7 @@ public class EditorFormController {
             while ((line = bufferedReader.readLine()) != null) {
                 txtEditor.appendText(line + '\n');
             }
+            lblApplicationStatus.setText("Successfully opened the file.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -259,11 +254,13 @@ public class EditorFormController {
             try (FileWriter fw = new FileWriter(absolutePath);
                  BufferedWriter bw = new BufferedWriter(fw)) {
                 bw.write(txtEditor.getText());
-                //System.out.println(txtEditor.getText());
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+        lblApplicationStatus.setText("File Successfully saved.");
     }
 
     public void btnSaveAs_OnAction(ActionEvent actionEvent) {
@@ -283,6 +280,7 @@ public class EditorFormController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        lblApplicationStatus.setText("File Successfully saved.");
     }
 
     public void mnuPrint_OnAction(ActionEvent actionEvent) {
@@ -293,21 +291,22 @@ public class EditorFormController {
     }
 
     public void mnuPageSetup_OnAction(ActionEvent actionEvent) {
-        //Window window = txtEditor.getScene().getWindow();
-        //System.out.println(window);
         printerJob.showPageSetupDialog(txtEditor.getScene().getWindow());
     }
 
     public void mnuCut_OnAction(ActionEvent actionEvent) {
         txtEditor.cut();
+        lblApplicationStatus.setText("| Cut completed");
     }
 
     public void mnuCopy_OnAction(ActionEvent actionEvent) {
         txtEditor.copy();
+        lblApplicationStatus.setText("| Copy completed");
     }
 
     public void mnuPaste_OnAction(ActionEvent actionEvent) {
         txtEditor.paste();
+        lblApplicationStatus.setText("| Paste completed");
     }
 
     public void mnuAbout_OnAction(ActionEvent actionEvent) throws IOException {
